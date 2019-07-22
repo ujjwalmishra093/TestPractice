7144200cc1e5ade7303bf7dd71e351ba0e8d63cc
Ujjwal Mishra <uk4119@gmail.com>
2019-07-22 15:40:26 +0530

    Initial Commit

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test{
+	public static void add(int num1,int num2){
+		int sum=0;
+		sum = num1+num2;
+		System.out.println("Sum Of Num1 And Num2 Is :: "+sum);
+	}
+	public static void main(String [] args){
+		add(10,20);
+	}
+}
